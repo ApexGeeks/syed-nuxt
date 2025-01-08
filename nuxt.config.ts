30c07069d44421491a331d8ce93520f57da476a8
Mugees Ahmad <shmugeesahmad@gmail.com>
2025-01-08 17:49:25 +0500

    dx: add support for ignoring directories in `pages` folder
    - to ignore any directory just add `_` before its name
    - Credits: https://github.com/nuxt/nuxt/issues/11497#issuecomment-2404063636

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
+import type { NuxtPage } from 'nuxt/schema';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint']
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+	hooks: {
+		'pages:extend': function (pages) {
+			function removePagesMatching(pattern: RegExp, pages: NuxtPage[] = []) {
+				const pagesToRemove: NuxtPage[] = [];
+
+				for (const page of pages) {
+					if (page.file?.match(pattern)) {
+						pagesToRemove.push(page);
+					} else {
+						removePagesMatching(pattern, page.children);
+					}
+				}
+
+				for (const page of pagesToRemove) {
+					pages.splice(pages.indexOf(page), 1);
+				}
+			}
+
+			removePagesMatching(/\/_.*?/, pages);
+		}
+	}
 });
