34b525a6b6adea4f3912d1dac07e43e5e56ee9e7
Soman-Dev <cybertech0.001s@gmail.com>
2025-02-08 20:43:24 +0500

    fix: Adjust the padding issues.

---
+++ pages/support/_support-components/SupportConnect.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+</script>
+
+<template>
+	<section class="bg-gray-200 pt-20 pb-10 md:px-6 lg:px-24">
+		<div class="container">
+			<div
+				class="mt-16 flex items-start justify-start gap-5 max-sm:flex-col md:items-center md:justify-between"
+			>
+				<h1 class="text-xl font-semibold max-sm:mb-5 max-sm:ml-16 md:text-4xl">
+					Let's
+					<span
+						class="border-l-4 border-blue-400 bg-gradient-to-tr from-blue-200 to-blue-50 pr-5 pl-1"
+					>
+						Connect
+					</span>
+				</h1>
+				<p
+					class="text-start text-[15px] font-medium max-sm:pr-8 max-sm:pl-10 md:max-w-72"
+				>
+					We're here to answer your questions and discuss how we can help you
+					achieve your goals.
+				</p>
+			</div>
+			<div class="mt-16 rounded-3xl bg-[#fbfbfb] py-6 max-sm:pl-6">
+				<CalendlyInlineWidget :url="runtimeConfig.public.calendly" />
+			</div>
+		</div>
+	</section>
+</template>
---
+++ pages/support/_support-components/SupportHero.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+</script>
+
+<template>
+	<div class="">
+		<div
+			class="font-Poppins text-Black container flex flex-col items-center py-20"
+		>
+			<img
+				src="/images/heading.png"
+				alt=""
+				class="max-w-64 cursor-pointer transition-all hover:scale-110"
+			/>
+			<h3 class="my-5 mt-16 text-lg font-semibold">
+				Helping Businesses Transition to AI
+			</h3>
+			<h1
+				class="font-Poppins max-w-xl text-center text-4xl leading-snug font-bold md:text-7xl"
+			>
+				We'll
+				<span
+					class="border-Green from-GreenLight border-l-4 bg-gradient-to-r to-transparent pl-2"
+				>
+					support
+				</span>
+				<p class="max-w-2xl">you all the way.</p>
+			</h1>
+			<div class="mt-5 gap-3 *:my-3 max-sm:*:mx-auto max-sm:*:w-full sm:flex">
+				<a
+					:href="runtimeConfig.public.calendly"
+					target="_blank"
+				>
+					<Button
+						class="inline-flex items-center justify-center gap-2 shadow-lg shadow-gray-300 max-sm:w-full"
+						><img
+							src="https://cdn.prod.website-files.com/66b3f7d1ff69e1e7133402fc/66b3f7d1ff69e1e713340363_Phone.svg"
+							alt=""
+						/>Book A Call</Button
+					>
+				</a>
+				<a href="#pricing">
+					<Button
+						is-light
+						class="max-sm:mt-3 max-sm:w-full max-sm:text-start"
+						>Discover Pricing</Button
+					>
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
